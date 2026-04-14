696d1889206b38ade53934e23d517ef425ec6828
dagmawisamuel057-hue <dagmawisamuel057@gmail.com>
2026-04-14 13:03:56 -0700

    add sql file

--- pre-week-2/Ex1B_first_query.sql
+++ pre-week-2/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about films, actors, customers, rentals, and inventory for use by a movie rental business.
-*/
+*/USE sakila;
+SELECT * FROM actor;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- 1000 records
