15afb59c076fc576e523683437f42f88708ed179
dagmawisamuel057-hue <dagmawisamuel057@gmail.com>
2026-04-17 08:48:07 -0700

    add  file

---
+++ pre-week-2/mulugeta_Ex4B_update_delete.sql
@@ -0,0 +1,15 @@
+UPDATE Customers
+SET phone = '555-000-1111'
+WHERE customer_id = 1;
+SELECT * FROM Customers;
+UPDATE Walks
+SET status = 'Completed'
+WHERE walk_id = 2;
+SELECT * FROM Walks;
+DELETE FROM Walks
+WHERE walk_id = 4;
+SELECT * FROM Walks;
+SELECT * FROM Walks;
+UPDATE Customers
+SET phone = '555-000-1111'
+WHERE customer_id = 1;
