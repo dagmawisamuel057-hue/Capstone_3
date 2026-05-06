d564a070344ac7207a2f3c303accc1f3ca1a4d5e
dagmawisamuel057-hue <dagmawisamuel057@gmail.com>
2026-05-06 08:50:00 -0700

    Ex3A_ConversionScripts

---
+++ pre-week-5/Ex3A_ConversionScripts/input_conversion.py
@@ -0,0 +1,9 @@
+# Input Conversion Example
+
+name = input("Dagmawi mulugeta: ")
+age = input("27: ")
+
+converted_age = int(age)
+
+print("Hello " + name)
+print("Next year you will be " + str(converted_age + 1))
---
+++ pre-week-5/Ex3A_ConversionScripts/numeric_conversion.py
@@ -0,0 +1,30 @@
+# Numeric Conversion Exercises
+
+# Integer conversion
+age = "27"
+converted_age = int(age)
+
+print("Original value:", age)
+print("Converted to integer:", converted_age)
+
+# Float conversion
+price = "19.99"
+converted_price = float(price)
+
+print("Original value:", price)
+print("Converted to float:", converted_price)
+
+# String conversion
+number = 100
+converted_number = str(number)
+
+print("Original number:", number)
+print("Converted to string:", converted_number)
+
+# Type checking
+print(type(age))
+print(type(converted_age))
+print(type(price))
+print(type(converted_price))
+print(type(number))
+print(type(converted_number))
